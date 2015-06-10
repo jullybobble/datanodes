1ea953424d4b44949bb24cfaa234c0c0ad2a9a1f
Julien Gaugaz <julien.gaugaz@gmail.com>
2015-06-10 23:32:43 +0200

    removed test-dependency on dplyr and wakefield

--- tests/testthat/test_datanode.R
+++ tests/testthat/test_datanode.R
@@ -1,17 +1,24 @@
-require(wakefield, warn.conflicts = F)
-require(dplyr, warn.conflicts = F)
+test_data <-
+  data.frame(ID = c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10"),
+             Age = c(26L, 25L, 23L, 32L, 24L, 20L, 29L, 24L, 21L, 33L),
+             Sex = factor(c("Male", "Female", "Male", "Female", "Male", "Male",
+                          "Female", "Female", "Female", "Female")),
+             Height = c(68, 70, 71, 73, 68, 66, 70, 71, 74, 70),
+             Died = c(TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE),
+             Date = as.Date(c("2014-07-10", "2014-10-10", "2014-11-24", "2014-11-10", "2014-12-10",
+                              "2014-12-10", "2015-01-10", "2015-03-10", "2015-05-10", "2015-05-14")),
+             stringsAsFactors = F)
 
-random_data <- function() r_data_frame(n = 10, id,  age, sex, height, died, date_stamp)
 csv_read_args <- list(col_types = list(ID = col_character(),
                                        Height = col_double(),
                                        Sex = col_factor(c("Female", "Male"))))
 
 test_that("dn create file if it doesn't exist", {
   file <- tempfile()
-  data <- random_data()
-  file.exists(file) %>% expect_false()
+  data <- test_data
+  expect_false(file.exists(file))
   returned <- datanode(file, data)
-  file.exists(file) %>% expect_true()
+  expect_true(file.exists(file))
 
   # clean file
   file.remove(file)
@@ -19,9 +26,9 @@ test_that("dn create file if it doesn't exist", {
 
 test_that("dn returns identical object when missed", {
   file <- tempfile()
-  data <- random_data()
-  expected <- data %>% bind_rows(data)
-  returned <- datanode(file, data %>% bind_rows(data))
+  data <- test_data
+  expected <- rbind(data, data)
+  returned <- datanode(file, rbind(data, data))
   expect_equal(returned, expected)
 
   # clean file
@@ -30,11 +37,11 @@ test_that("dn returns identical object when missed", {
 
 test_that("dn returns identical object when hit", {
   file <- tempfile()
-  data <- random_data()
-  expected <- data %>% bind_rows(data)
-  datanode(file, data %>% bind_rows(data))
-  file.exists(file) %>% expect_true()
-  returned <- datanode(file, data %>% bind_rows(data))
+  data <- test_data
+  expected <- rbind(data, data)
+  datanode(file, rbind(data, data))
+  expect_true(file.exists(file))
+  returned <- datanode(file, rbind(data, data))
   expect_equal(returned, expected)
 
   # clean file
@@ -43,12 +50,12 @@ test_that("dn returns identical object when hit", {
 
 test_that("dn doesn't evaluate expression when hit", {
   file <- tempfile()
-  expected <- datanode(file, random_data())
-  file_time <- file.info(file) %>% with(mtime)
+  expected <- datanode(file, test_data)
+  file_time <- file.info(file)$mtime
 
   returned <- datanode(file, stop())
   expect_equal(returned, expected)
-  expect_identical(file.info(file) %>% with(mtime),
+  expect_identical(file.info(file)$mtime,
                    file_time)
 
   # clean file
@@ -57,10 +64,10 @@ test_that("dn doesn't evaluate expression when hit", {
 
 test_that("dn respects `force` parameter", {
   file <- tempfile()
-  data <- random_data()
+  data <- test_data
   expected <- data
   datanode(file, data)
-  file.exists(file) %>% expect_true()
+  expect_true(file.exists(file))
   evaluated = F
   datanode(file, force = F, {
     evaluated <- T
@@ -79,28 +86,28 @@ test_that("a data node gets triggered by `file_time` with file dependency", {
 
   # dependency
   dep_file <- tempfile()
-  dep <- datanode(dep_file, random_data())
+  dep <- datanode(dep_file, test_data)
 
   # tested node
   file <- tempfile()
-  dep_fun <- function(data) bind_rows(data, data)
+  dep_fun <- function(data) rbind(data, data)
   node <- datanode(file,
                    depends_on = dep_file, {
     dep_fun(dep)
   })
-  file_time_1 <- file.info(file) %>% with(mtime)
+  file_time_1 <- file.info(file)$mtime
 
   Sys.sleep(1) # makes sure file system time resolution is
                  # sufficient (1s should be enough everywhere)
 
   # test miss
-  dep_2 <- datanode(dep_file, random_data(), force = T)
+  dep_2 <- datanode(dep_file, test_data, force = T)
   node <- datanode(file,
                    depends_on = dep_file, {
     dep_fun(dep_2)
   })
   expect_less_than(file_time_1,
-                   file.info(file) %>% with(mtime))
+                   file.info(file)$mtime)
   expect_equivalent(dep_fun(dep_2),
                     node)
 
@@ -113,7 +120,7 @@ test_that("rdata io reads what was written", {
   f <- tempfile()
   on.exit(file.remove(f))
 
-  data <- random_data()
+  data <- test_data
 
   datanode(f, io = rdata_io, data)
 
@@ -126,8 +133,7 @@ test_that("csv io reads what was written", {
   f <- tempfile()
   on.exit(file.remove(f))
 
-  data <- random_data() %>%
-    as.data.frame() # all.equal.tbl_df has problems with Date after csv write-read
+  data <- test_data
 
   datanode(f, io = csv_io, data)
 
@@ -144,8 +150,7 @@ test_that("csv io is selected if file ends in '.csv'", {
   f <- tempfile(fileext = '.cSv')
   on.exit(file.remove(f))
 
-  data <- random_data() %>%
-    as.data.frame() # all.equal.tbl_df has problems with Date after csv write-read
+  data <- test_data
 
   datanode(f, data)
 
@@ -159,8 +164,7 @@ test_that("RData io is selected if file ends in '.RData'", {
   f <- tempfile(fileext = '.rDaTa')
   on.exit(file.remove(f))
 
-  data <- random_data() %>%
-    as.data.frame() # all.equal.tbl_df has problems with Date after csv write-read
+  data <- test_data
 
   datanode(f, data)
 
@@ -172,8 +176,7 @@ test_that("RDS io is selected if file ends in 'rds'", {
   f <- tempfile(fileext = '.rdS')
   on.exit(file.remove(f))
 
-  data <- random_data() %>%
-    as.data.frame() # all.equal.tbl_df has problems with Date after csv write-read
+  data <- test_data
 
   datanode(f, data)
 
